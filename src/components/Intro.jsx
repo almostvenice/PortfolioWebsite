b44fc98d5ba6cf8d4047a5470126e1d6adb97df4
David Preciado <almostvenice@gmail.com>
2022-06-21 15:23:36 -0700

    Scrolling Greeting

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,29 @@
-
+import './intro.css';
+import {Col, Row, Container } from 'reactstrap';
 
 
 const Intro = () => {
     return ( 
-        <>
-            PlaceHolder
-        </>
+        <Container>
+            <Row className='row-content align-items-center'>
+                <Col sm="8">
+                    <div className="left-wrapper">
+                        <h2 className='introGreet'>Hi,</h2>
+                        <h1 className='introName'>I'm Bob,</h1>
+                        <div className="intro-title">
+                            <div className="title-wrapper">
+                                <div className="title-item">React Developer</div>
+                                <div className="title-item">Java Developer</div>
+                                <div className="title-item">Web3 Enthusiast</div>
+                            </div>
+                        </div>
+                    </div>
+                </Col>
+                <Col sm='4'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sit ex fugiat magnam quas at harum inventore facere praesentium nulla!
+                </Col>
+            </Row>
+        </Container>
      );
 }
  
