421c97017a105eec153811ace6e6d951fcb49fc5
James Allard <jd.allard@aol.com>
2022-07-06 18:39:26 -0500

    form animation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,19 +1,33 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FormGroup, Label, Input, Button, Form, FormText } from "reactstrap";
+import { FormGroup, Label, Input, Button, Form, FormText, Row } from "reactstrap";
 import "./contactForm.css";
+import {Player, Controls} from '@lottiefiles/react-lottie-player';
 function ContactForm() {
   return (
-      <div style={{
-            display: "block",
-            width: 550,
-            height: 475,
-            padding: 30
-          }} 
-          className="bor"
-      >
-        <h5 className="text-white text-center">Contact Me For An Interview</h5>
+
+
+    <div style={{
+      display: "block",
+      width: 550,
+      height: 610,
+      padding: 30
+    }} 
+    className="bor"
+    >
+        
+   
+      <Player 
+       id='lottiePlayer'
+       autoplay
+       loop
+       src="https://assets1.lottiefiles.com/packages/lf20_gaplvsns.json"
+       style={{ height: '150px', width: '150px', float: '', }}
+>
+    <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+</Player>
+  <h5 className="text-white text-center">Contact Me For An Interview</h5>
         <Form >
           <FormGroup>
             <Label htmlFor="name" className="text-white">
@@ -41,7 +55,8 @@ function ContactForm() {
             <Label htmlFor="message" className="text-white">Message:</Label>
             <Input type="textarea" name="text" id="message" rows="5" cols='40' placeholder="Tell Me About The Opportunity" />
           </FormGroup>
-          <Button style={{float: 'right'}}>Submit</Button>
+         
+          <Button style={{float: 'right', }}>Submit</Button>
         </Form>
       </div>
   );
